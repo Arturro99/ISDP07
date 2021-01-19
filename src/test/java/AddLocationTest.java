3eca77bdb36d11c2221c7d779f2c491bbbe8015b
Student IT <kacper.swiercz00@gmail.com>
2021-01-19 18:47:45 +0100

    Remove IntegrationTests class

--- src/test/java/AddLocationTest.java
+++ src/test/java/AddLocationTest.java
@@ -1,6 +1,4 @@
 
-import java.util.ArrayList;
-import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -53,7 +51,7 @@ public class AddLocationTest {
     
     @AfterClass(alwaysRun = true)
     public void end() {
-        //driver.close();
+        driver.close();
     }
     
 }
