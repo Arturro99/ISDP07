ee4f7266bbabe76b20f4624c5fa08c2866d7eef0
root <kacper.swiercz00@gmail.com>
2021-01-19 12:40:50 +0100

    Add integration tests

---
+++ src/test/java/IntegrationTests.java
@@ -0,0 +1,17 @@
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+        
+public class IntegrationTests {
+    @Test
+    public void test1() {
+        Assert.assertTrue(true);
+    }
+    
+    @Test
+    public void test2() {
+        Assert.assertTrue(false);
+    }
+}
